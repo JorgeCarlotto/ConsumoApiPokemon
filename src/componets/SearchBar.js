ce95dbfdd85e677c64f9777df8c7ec6f5aa5550d
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-01-10 16:02:52 -0300

    successful connection with api

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -7,16 +7,18 @@ const {useState} = React;
 const SearchBar = ()=>{
 
     const [searchPoke ,setsearchPoke] = useState('');
+    const [pokemon , setpokemon] = useState();
 
     const onChange = (e)=>{
-        console.log(e)
-        console.log(e.target.value);
+        // console.log(e)
+        // console.log(e.target.value);
         setsearchPoke( e.target.value);
     };
 
     const onClick = async (e)=>{
         const data = await searchPokemon(searchPoke)
-        console.log(data);
+        setpokemon(data);
+        console.log(data)
     };          
     return(
         <div>
@@ -26,7 +28,16 @@ const SearchBar = ()=>{
             <div>
                <button onClick={onClick}>Buscar Pokemon</button>
             </div>
+            <div>
+                {pokemon &&
+            <div>
+            <div>Nombre: {pokemon.name}</div>
+            <div>Peso: {pokemon.weight}</div>
+            <img src ={pokemon.sprites.front_default} />
+            </div>
+                }
             
+            </div>
         </div>
     )
 };
